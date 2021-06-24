72948fb87dac91be7a7f07a51525b805433556a6
speedyz <99speedyz@gmail.com>
2021-06-24 08:44:38 +0300

    add project github

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Tree :label="tree.label"
+              :nodes="tree.nodes"
+              :depth="0"
+    ></Tree>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+let tree = {
+  label: 'Food',
+  nodes: [
+    {
+      label: 'Plant',
+      nodes: [
+        {
+          label: 'Fruits',
+          nodes:[
+            {
+              label: 'Apples'
+            },
+            {
+              label: 'Mangos'
+            },
+            {
+              label: 'Oranges'
+            },
+          ]
+        },
+        {
+          label: 'Vegetables',
+          nodes: [
+            {
+              label: 'Tomatoes'
+            },
+            {
+              label: 'Potatoes'
+            },
+            {
+              label: 'Cucumber'
+            },
+          ]
+        }
+      ]
+    },
+    {
+      label: 'Animal',
+      nodes: [
+        {
+          label: 'Meat'
+        },
+        {
+          label: 'Milk'
+        },
+        {
+          label: 'Eggs'
+        },
+      ]
+    }
+  ]
+};
 
+
+
+import Tree from '@/components/Tree'
 export default {
-  name: 'App',
+  name: 'app',
+  data () {
+    return {
+      tree
+    }
+  },
   components: {
-    HelloWorld
+    Tree
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Tree.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="tree-menu">
+    <button class="label-wrapper" :style="indent" @click="toggleChildren">{{ label }}</button>
+    <div class="line">
+    <Tree
+        v-show="showChildren"
+        v-for="(node, ind) in nodes"
+        :nodes="node.nodes"
+        :label="node.label"
+        :depth="depth + 1"
+        :key="ind"
+    >
+    </Tree>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['label', 'nodes', 'depth'],
+  name: 'Tree',
+  data() {
+    return {
+      showChildren: false
+    }
+  },
+  computed: {
+    indent() {
+      return {transform: `translate(${this.depth * 100}px)`}
+    }
+  },
+  methods: {
+    toggleChildren() {
+      this.showChildren = !this.showChildren
+    }
+  }
+}
+</script>
+<style>
+.label-wrapper {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 20px;
+  margin-bottom: 15px;
+  cursor: pointer;
+  }
+</style>
+
